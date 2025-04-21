b64179e110fb95e7a21c6ed694bb5e3eed1d8b6f
prostoSania <cemkatvsuper@gmail.com>
2025-04-22 00:46:36 +0300

    Добавьте файлы проекта.

---
+++ proect_2/proect_2/Core/CoreConnection.cs
@@ -0,0 +1,18 @@
+﻿using DurableTask.Core.Common;
+using proect_2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace proect_2.Core
+{
+    internal static class CoreConnection
+    {
+        public static Frame CoreFrame { get; set; }
+
+        public static Entities1 DB { get; set; }
+    }
+}
---
+++ proect_2/proect_2/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using DurableTask.Core.Common;
+using proect_2.Core;
+using proect_2.Model;
+using proect_2.View.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace proect_2.View
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CoreConnection.CoreFrame = MainFrame;
+            CoreConnection.DB = new Entities1();
+            MainFrame.Navigate(new MainLoginPage());
+        }
+    }
+}
